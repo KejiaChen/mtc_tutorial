3fbb7bf04c2d14c5004da2a9cc47158dcca2e247
KejiaChen <kejia.chen@tum.de>
2025-05-20 21:44:37 +0200

    add ompl and chomp configuration

--- launch/pick_place_demo_dual.launch.py
+++ launch/pick_place_demo_dual.launch.py
@@ -3,7 +3,30 @@ from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
 
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
 
 def generate_launch_description():
     use_sensone_left = DeclareLaunchArgument(
@@ -30,6 +53,10 @@ def generate_launch_description():
     #     default_value="false",  # Default file path
     #     description="Whether to use path constraints",
     # )  
+
+    # Planning Configuration
+    ompl_planning_yaml = load_yaml("dual_arm_panda_moveit_config", "config/ompl_planning.yaml")
+    chomp_planning_yaml = load_yaml("dual_arm_panda_moveit_config", "config/chomp_planning.yaml")
     
     moveit_config = (
         MoveItConfigsBuilder("dual_arm_panda")
@@ -49,7 +76,7 @@ def generate_launch_description():
             },
         )
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
-        .planning_pipelines(pipelines=["ompl"])
+        .planning_pipelines(pipelines=["ompl", "chomp"])
         .joint_limits(file_path="config/joint_limits.yaml")
         # .pilz_cartesian_limits(file_path="config/pilz_cartesian_limits.yaml")
         .to_moveit_configs()
@@ -67,6 +94,8 @@ def generate_launch_description():
             moveit_config.to_dict(),
             {"use_sensone_left": LaunchConfiguration("use_sensone_left")},
             {"use_sensone_right": LaunchConfiguration("use_sensone_right")},
+            ompl_planning_yaml,
+            chomp_planning_yaml,
         ],
         # arguments=[
         #     # LaunchConfiguration("constraints")
